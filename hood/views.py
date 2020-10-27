021a317039cbca00a4581ec5f95cc92393ed0fcd
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-27 09:19:51 +0300

    a view for the profile

--- hood/views.py
+++ hood/views.py
@@ -102,3 +102,8 @@ def leave_hood(request, id):
     request.user.profile.neighbourhood = None
     request.user.profile.save()
     return redirect('hood')
+
+
+
+def profile(request, username):
+    return render(request, 'profile.html')

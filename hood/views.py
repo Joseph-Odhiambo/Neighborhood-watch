96f1b9eafd91f1e999dc0e352546a20433af73f7
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-27 08:28:03 +0300

    sign up rout

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
+from .forms import SignupForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})

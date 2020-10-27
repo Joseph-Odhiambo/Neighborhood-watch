6ba86bb2747796d7596f286f2d8611a7be68984c
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-27 08:16:08 +0300

    index view and html

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')

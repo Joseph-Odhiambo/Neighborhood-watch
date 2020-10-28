e3450dae1049a4fb7e5644d07494d4b17d5864a4
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-28 09:10:28 +0300

    deployment

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
+
 # ALLOWED_HOSTS = []
 DEBUG = config('DEBUG', default=False, cast=bool)
 

3f181adaf8eb2a7f028351b4c19efd7a9b7bb4c5
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-28 06:59:05 +0300

    added readme license files

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config, Csv
+# from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'm-r(qp_u)%tob405#&b&+ehx^_0uy3*usavj@e8znd+k%8!u7e'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'm-r(qp_u)%tob405#&b&+ehx^_0uy3*usavj@e8znd+k%8!u7e'
+# SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
